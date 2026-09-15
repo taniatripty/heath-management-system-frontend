6df845e892e79d26a1fa548e78a60941d0f0bacd
taniatripty <taniatripty381@gmail.com>
2026-09-15 21:06:51 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+
+
+export default function Home() {
+  return (
+   <div>
+   <p>
+    hellow
+   </p>
+   <Button>hellew</Button>
+  
+   </div>
+  );
+}
