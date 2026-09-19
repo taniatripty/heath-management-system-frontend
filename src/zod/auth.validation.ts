c6341e457aba3f7d96cac264329651025068e890
taniatripty <taniatripty381@gmail.com>
2026-09-19 15:10:05 +0600

    add loginForm page

--- src/zod/auth.validation.ts
+++ src/zod/auth.validation.ts
@@ -1,8 +1,8 @@
 import z from "zod";
 
-export const zodloginSchema = z.object({
+export const loginZodSchema = z.object({
     email: z.email({ message: "Invalid email address" }),
     password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
 });
 
-export type ILogInpayload = z.infer<typeof zodloginSchema>;
+export type ILogInpayload = z.infer<typeof loginZodSchema>;
