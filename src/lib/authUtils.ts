0034e0dd41f2b3fb279b80979e0cd3e960878121
taniatripty <taniatripty381@gmail.com>
2026-09-21 23:50:07 +0600

    add authUtils file

---
+++ src/lib/authUtils.ts
@@ -0,0 +1,54 @@
+export type UserRole ="SUPER_ADMIN" | "ADMIN" | "DOCTOR" | "PATIENT"
+
+export const authRoutes=["/login","/register","/forgetPassword","/resetPass", "/verrifyEmail"] 
+
+export const isAuthRoute=(pathname:string)=>{
+    return authRoutes.some((router:string)=>router===pathname)
+
+}
+
+export type RouteConfig={
+    exact:string[],
+    pattern:RegExp[]
+}
+
+export const commonRouteConfig:RouteConfig={
+    exact:["/changePassword","/myProfile"],
+    pattern:[]
+}
+
+export const doctorProtectedRoutes:RouteConfig={
+    pattern:[/^\/doctor\/dashboard/],
+    exact:[]
+}
+export const adminProtectedRoutes:RouteConfig={
+    pattern:[/^\/admin\/dashboard/ ],
+    exact:[]
+}
+export const patientProtectedRoutes : RouteConfig = {
+    pattern: [/^\/dashboard/ ], // Matches any path that starts with /dashboard
+    exact : [ "/payment/success"]
+};
+
+export const isRoutesMatches=(pathName:string, routes:RouteConfig)=>{
+if(routes.exact.includes(pathName)){
+    return true
+}
+return routes.pattern.some((pattern:RegExp)=>pattern.test(pathName))
+}
+
+export const getRouteOwner=(pathname:string):"SUPER_ADMIN" | "ADMIN" | "DOCTOR" | "PATIENT" | "COMMON" | null=>{
+if(isRoutesMatches(pathname,doctorProtectedRoutes)){
+    return "DOCTOR"
+}
+if(isRoutesMatches(pathname,adminProtectedRoutes)){
+    return "ADMIN"
+}
+if(isRoutesMatches(pathname,patientProtectedRoutes)){
+    return "PATIENT"
+}
+if (isRoutesMatches(pathname,commonRouteConfig)){
+    return "COMMON"
+}
+ return null; 
+}
