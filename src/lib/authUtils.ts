092170389814a719d7011d34ba75f0f71f0193a8
taniatripty <taniatripty381@gmail.com>
2026-09-21 23:55:15 +0600

    modify authUtils file

--- src/lib/authUtils.ts
+++ src/lib/authUtils.ts
@@ -52,3 +52,17 @@ if (isRoutesMatches(pathname,commonRouteConfig)){
 }
  return null; 
 }
+
+export const getDefaultDashboardRoute = (role :UserRole) => {
+    if(role === "ADMIN" || role === "SUPER_ADMIN") {
+        return "/admin/dashboard";
+    }
+    if(role === "DOCTOR") {
+        return "/doctor/dashboard";
+    }
+    if(role === "PATIENT") {
+        return "/dashboard";
+    }
+
+    return "/";
+}
